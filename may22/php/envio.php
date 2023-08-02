7962f238098c2a2d6f60d166d1c72c82493ed5fd
M4t3B4rriga <msbarriga@espe.edu.ec>
2023-08-02 10:47:13 -0500

    may22

---
+++ may22/php/envio.php
@@ -0,0 +1,34 @@
+<?php 
+$nombre=$_POST['nom'];
+$apellido=$_POST['ape'];
+$correo=$_POST['correo'];
+$url=$_POST['url'];
+$fecha=$_POST['Fecha'];
+$fechaActual=$_POST['actual'];
+$sueldo=$_POST['sueldo'];
+$telefono=$_POST['tel'];
+$color=$_POST['color'];
+
+echo("Datos del formulario: ");
+echo("<br>");
+echo("Nombre: ".$nombre);
+echo("<br>");
+echo("Apellido: ".$apellido);
+echo("<br>");
+echo("Correo: ".$correo);
+echo("<br>");
+echo("URL: ".$url);
+echo("<br>");
+echo("Fecha: ".$fecha);
+echo("<br>");
+echo("Fecha Actual: ".$fechaActual);
+echo("<br>");
+echo("Sueldo: ".$sueldo);
+echo("<br>");
+echo("Telefono: ".$telefono);
+echo("<br>");
+echo("Color: ".$color);
+
+
+
+?>
